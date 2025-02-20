9c0b8eae65b49e79219f8d258a7c6212a3890318
jorge <jorge>
2025-02-20 21:57:01 +0100

    update

--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -5,7 +5,10 @@
   import MainView from "./components/MainView.svelte";
 </script>
 
-<div id="layout" class="h-screen grid grid-flow-col overflow-hidden">
+<div
+  id="layout"
+  class="h-screen grid grid-flow-col grid-cols-[min-content_1fr] overflow-hidden"
+>
   <!-- Antes: grid-cols-[12.5rem_1fr]  -->
   <Sidebar></Sidebar>
   <MainView></MainView>
