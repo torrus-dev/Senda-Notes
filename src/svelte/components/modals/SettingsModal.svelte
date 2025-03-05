6838ba078d1c29444b86963a18ce656a28d3af27
jorge <jorge>
2025-03-05 13:05:46 +0100

    update

--- src/svelte/components/modals/SettingsModal.svelte
+++ src/svelte/components/modals/SettingsModal.svelte
@@ -1,10 +1,31 @@
 <script>
 import { SearchIcon } from "lucide-svelte";
-let selectedTab = 0;
 let searchQuery = $state("");
+let settingsData = {
+  App: [
+    {
+      id: "language",
+      title: "Lenguaje",
+      description: "Cambiar lenguaje de la app",
+      type: "select",
+      options: ["Spanish", "English"],
+      defaultValue: "English",
+    },
+  ],
+  Apariencia: [
+    {
+      id: "themeMode",
+      title: "Modo Claro / Oscuro",
+      description: "",
+      type: "select",
+      options: ["Light", "Dark"],
+      defaultValue: "System",
+    },
+  ],
+};
 </script>
 
-<aside>
+<aside class="flex gap-2">
   <div class="bg-base-400 flex items-center rounded px-2 py-1">
     <SearchIcon class="h-5 w-5 text-gray-500" />
     <input
@@ -12,8 +33,8 @@ let searchQuery = $state("");
       name="search"
       class="ml-2 flex-1 bg-transparent focus:outline-none"
       bind:value={searchQuery}
-      placeholder="Buscar..." />
+      placeholder="Search..." />
   </div>
-  <h3>Configuración</h3>
+  <h3>Settings</h3>
 </aside>
 <main></main>
