1bd13f4e55da99c3cd11604240386ec4ab523ade
Torrus <Torrus@torrus.dev>
2025-03-13 10:38:33 +0100

    update

--- src/svelte/components/layout/ContextMenu.svelte
+++ src/svelte/components/layout/ContextMenu.svelte
@@ -17,38 +17,40 @@ $effect(() => {
 let position = $derived(contextMenuController.getAdaptedPosition());
 </script>
 
-<ul
-  bind:this={menuElement}
-  id="context-menu"
-  role="menu"
-  aria-orientation="vertical"
-  tabindex="-1"
-  use:closeOnOutsideOrEsc={() => {
-    contextMenuController.close();
-  }}
-  class="rounded-box bordered bg-base-200 absolute z-20 mt-1 p-2 shadow"
-  style="left: {position.x}px; top: {position.y}px;">
-  {#each contextMenuController.menuItems as item, i}
-    {#if item.separator}
-      <li class="border-border-normal my-1 border-t-2" role="separator"></li>
-    {:else}
-      <li>
-        <Button
-          onclick={() => {
-            if (item.onClick) {
-              item.onClick();
-            }
-            contextMenuController.close();
-          }}
-          role="menuitem"
-          tabindex="0"
-          cssClass="w-full {item.class || ''}">
-          {#if item.icon}
-            <item.icon size="18"></item.icon>
-          {/if}
-          {item.label}
-        </Button>
-      </li>
-    {/if}
-  {/each}
-</ul>
+{#if contextMenuController.isOpen}
+  <ul
+    bind:this={menuElement}
+    id="context-menu"
+    role="menu"
+    aria-orientation="vertical"
+    tabindex="-1"
+    use:closeOnOutsideOrEsc={() => {
+      contextMenuController.close();
+    }}
+    class="rounded-box bordered bg-base-200 absolute z-20 mt-1 p-2 shadow"
+    style="left: {position.x}px; top: {position.y}px;">
+    {#each contextMenuController.menuItems as item, i}
+      {#if item.separator}
+        <li class="border-border-normal my-1 border-t-2" role="separator"></li>
+      {:else}
+        <li>
+          <Button
+            onclick={() => {
+              if (item.onClick) {
+                item.onClick();
+              }
+              contextMenuController.close();
+            }}
+            role="menuitem"
+            tabindex="0"
+            cssClass="w-full {item.class || ''}">
+            {#if item.icon}
+              <item.icon size="18"></item.icon>
+            {/if}
+            {item.label}
+          </Button>
+        </li>
+      {/if}
+    {/each}
+  </ul>
+{/if}
