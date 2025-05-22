2be3aca444419f7d175df649500666d4c0fb92f4
Torrus <Torrus@dev>
2025-05-22 14:59:17 +0200

    Comenzar con los nuevos componentes para el sistema de propiedades actualizado

--- src/svelte/components/note/properties/NewProperty.svelte
+++ src/svelte/components/note/properties/NewProperty.svelte
@@ -1,18 +1,40 @@
 <script lang="ts">
 import PropertyIcon from "@components/note/properties//PropertyIcon.svelte";
+
 import type { Note } from "@projectTypes/noteTypes";
+import type { GlobalProperty } from "@projectTypes/propertyTypes";
+
 import PropertyNameInput from "@components/note/properties/PropertyNameInput.svelte";
+import { notePropertyController } from "@controllers/note/property/notePropertyController.svelte";
+import { workspace } from "@controllers/workspaceController.svelte";
 
 let { noteId }: { noteId: Note["id"] } = $props();
 
-function handlePropertyRename() {}
-function handleSelectGlobalProperty() {}
+const newPropertyType = "text";
+
+function createPropertyFromName(propertyname: string) {
+   notePropertyController.handleCreateNoteProperty(
+      noteId,
+      propertyname,
+      newPropertyType,
+   );
+   workspace.stopPropertyEdit();
+}
+function createPropertyFromGlobal(globalProperty: GlobalProperty) {
+   notePropertyController.handleCreateNoteProperty(
+      noteId,
+      globalProperty.name,
+      globalProperty.type,
+   );
+   workspace.stopPropertyEdit();
+}
 </script>
 
 <div class="grid grid-cols-[12rem_auto] gap-0.5">
-   <PropertyIcon propertyType={"text"} />
+   <PropertyIcon propertyType={newPropertyType} />
+   <!-- HACER QUE FUNCIONE ESTO DE ABAJO -->
    <PropertyNameInput
-      onchange={handlePropertyRename}
-      onSelectGlobalProperty={handleSelectGlobalProperty} />
-   noteId={noteId}
+      onchange={createPropertyFromName}
+      onSelectGlobalProperty={createPropertyFromGlobal}
+      noteId={noteId} />
 </div>
